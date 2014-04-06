9350d02317c5a028ee51d986c7a5e9deafd7f6fc
Jordi Reixach <reixowtf@reixowtf-PC>
2014-04-06 17:37:05 +0200

    tot fet

--- KrewellaApp/gen/com/example/krewellaapp/R.java
+++ KrewellaApp/gen/com/example/krewellaapp/R.java
@@ -8,7 +8,14 @@
 package com.example.krewellaapp;
 
 public final class R {
+    public static final class array {
+        public static final int ImatgesKrewella=0x7f070000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int Blanc=0x7f090001;
+        public static final int NegreTransparent=0x7f090000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -19,37 +26,89 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int foto_dimen=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_pause=0x7f020000;
+        public static final int button_play=0x7f020001;
+        public static final int fons=0x7f020002;
+        public static final int gravar=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int informacio=0x7f020005;
+        public static final int krew1=0x7f020006;
+        public static final int krew2=0x7f020007;
+        public static final int krew3=0x7f020008;
+        public static final int krew4=0x7f020009;
+        public static final int krew5=0x7f02000a;
+        public static final int krew6=0x7f02000b;
+        public static final int krew7=0x7f02000c;
+        public static final int krew8=0x7f02000d;
+        public static final int krew9=0x7f02000e;
+        public static final int reproduir=0x7f02000f;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f090000;
-        public static final int action_settings=0x7f090006;
-        public static final int btn_entrar=0x7f090005;
-        public static final int btn_ferFoto=0x7f090003;
-        public static final int eT_nom_cognom=0x7f090002;
-        public static final int imgV_fotografia=0x7f090004;
-        public static final int textView1=0x7f090001;
+        public static final int LinearLayout1=0x7f0b0000;
+        public static final int LinearLayout2=0x7f0b000b;
+        public static final int action_settings=0x7f0b0014;
+        public static final int btn_entrar=0x7f0b0008;
+        public static final int btn_ferFoto=0x7f0b0006;
+        public static final int btnplay=0x7f0b0012;
+        public static final int btnreproduir=0x7f0b0013;
+        public static final int container=0x7f0b0003;
+        public static final int eT_nom_cognom=0x7f0b0005;
+        public static final int gridView_krewellaImatges=0x7f0b0004;
+        public static final int imageBlog=0x7f0b000f;
+        public static final int imageGravar=0x7f0b0010;
+        public static final int imageVideo=0x7f0b000e;
+        public static final int imageViewReproduir=0x7f0b000c;
+        public static final int imatgePersona=0x7f0b0009;
+        public static final int imatge_ampliada=0x7f0b0001;
+        public static final int imgV_fotografia=0x7f0b0007;
+        public static final int player_view=0x7f0b0011;
+        public static final int salutacio_mes_nom=0x7f0b000a;
+        public static final int textViewNomMusica=0x7f0b000d;
+        public static final int text_imatge=0x7f0b0002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_gravacio=0x7f030000;
+        public static final int activity_imatge=0x7f030001;
+        public static final int activity_imatges=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_menu=0x7f030004;
+        public static final int activity_video=0x7f030005;
+        public static final int dialeg_gravacio=0x7f030006;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int foto=0x7f0a0000;
+        public static final int fotos=0x7f0a0001;
+        public static final int image_adapter=0x7f0a0002;
+        public static final int main=0x7f0a0003;
+        public static final int video=0x7f0a0004;
+        public static final int youtube=0x7f0a0005;
     }
     public static final class raw {
         public static final int ic_action_user=0x7f040000;
+        public static final int krewella=0x7f040001;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int cognoms=0x7f060004;
         public static final int entrar=0x7f060007;
+        public static final int escoltar=0x7f06000d;
         public static final int fer_foto=0x7f060006;
         public static final int hello_world=0x7f060002;
         public static final int nom=0x7f060003;
         public static final int nomiICognoms=0x7f060005;
+        public static final int opinio=0x7f06000e;
+        public static final int parar=0x7f06000c;
+        public static final int reproduir=0x7f06000b;
+        public static final int title_activity_foto=0x7f060011;
+        public static final int title_activity_fotos=0x7f06000f;
+        public static final int title_activity_gravacio=0x7f06000a;
+        public static final int title_activity_image_adapter=0x7f060010;
+        public static final int title_activity_video=0x7f060008;
+        public static final int title_activity_youtube=0x7f060009;
     }
     public static final class style {
         /** 
@@ -73,10 +132,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- KrewellaApp/src/com/example/krewellaapp/MainActivity.java
+++ KrewellaApp/src/com/example/krewellaapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.krewellaapp;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,12 +13,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.MediaStore.Images.Media;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -27,6 +30,9 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 
+	private String STATE_PHOTO = "fotografia_feta";
+	private String PHOTO_PATH = "path_fotografia";
+	private String NOM_COGNOMS = "nom_cognoms";
 	private Button ferFoto;
 	private Button entrar;
 	private ImageView imatge;
@@ -34,29 +40,52 @@ public class MainActivity extends Activity implements OnClickListener {
 	private boolean imatgeFeta;
 	static final int CAMERA_APP_CODE = 100;
 	private File tempImageFile;
+	private Bitmap bmp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		imatgeFeta = false;
-
+		if (savedInstanceState == null) {
+			imatgeFeta = false;
+		}
 		nomCognoms = (EditText) findViewById(R.id.eT_nom_cognom);
 		imatge = (ImageView) findViewById(R.id.imgV_fotografia);
 		ferFoto = (Button) findViewById(R.id.btn_ferFoto);
 		entrar = (Button) findViewById(R.id.btn_entrar);
-
+		
 		ferFoto.setOnClickListener(this);
 		entrar.setOnClickListener(this);
-
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putBoolean(STATE_PHOTO, imatgeFeta);
+		if (imatgeFeta) {
+			outState.putString(NOM_COGNOMS, nomCognoms.getText().toString());
+			outState.putString(PHOTO_PATH, tempImageFile.getAbsolutePath());
+		}
+		super.onSaveInstanceState(outState);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	@Deprecated
+	public Object onRetainNonConfigurationInstance() {
+		return bmp;
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		imatgeFeta = savedInstanceState.getBoolean(STATE_PHOTO);
+		if (imatgeFeta) {
+			nomCognoms.setText(savedInstanceState.getString(NOM_COGNOMS));
+			imatge = (ImageView) findViewById(R.id.imgV_fotografia);
+			imatge.setImageBitmap(bmp);
+			//canviarOrientacio();
+			imatgeFeta = true;
+		}
+		super.onRestoreInstanceState(savedInstanceState);
 	}
 
 	@Override
@@ -66,21 +95,22 @@ public class MainActivity extends Activity implements OnClickListener {
 			ferFoto();
 			break;
 		case R.id.btn_entrar:
-			if (nomCognoms.getText().toString().isEmpty()) {
-				Toast.makeText(this, "Falta entrar el nom i el cognom", Toast.LENGTH_SHORT).show();
-				/*if(nomCognoms.getText().toString().matches("[a-z|A-Z]+[\\s]*^[a-z|A-Z]+")){
-					Toast.makeText(this, "Falta entrar el cognom", Toast.LENGTH_SHORT).show();
-				}*/
-			} else if (imatgeFeta) {
-				Toast.makeText(this, "Falta fer la fotografia", Toast.LENGTH_SHORT).show();
-			} else{
-				//fer intent
+			if (nomCognoms.getText().toString().equalsIgnoreCase("")
+					|| nomCognoms.getText().toString().equalsIgnoreCase(null)) {
+				Toast.makeText(this, "Falta entrar el nom i el cognom",
+						Toast.LENGTH_SHORT).show();
+			} else if (!imatgeFeta) {
+				Toast.makeText(this, "Falta fer la fotografia",
+						Toast.LENGTH_SHORT).show();
+			} else {
+				Intent i = new Intent(this, MenuActivity.class);
+				i.putExtra("nom", nomCognoms.getText().toString());
+				i.putExtra("imatge", tempImageFile);
+				startActivity(i);
+				finish();
 			}
 			break;
-		default:
-			break;
 		}
-
 	}
 
 	public static boolean isIntentAvailable(Context context, String action) {
@@ -90,7 +120,7 @@ public class MainActivity extends Activity implements OnClickListener {
 				PackageManager.MATCH_DEFAULT_ONLY);
 		return list.size() > 0;
 	}
-	
+
 	private void ferFoto() {
 		if (isIntentAvailable(this, MediaStore.ACTION_IMAGE_CAPTURE)) {
 			Intent takePictureIntent = new Intent(
@@ -104,16 +134,18 @@ public class MainActivity extends Activity implements OnClickListener {
 					Toast.LENGTH_LONG).show();
 		}
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (requestCode == CAMERA_APP_CODE) {
 			if (resultCode == RESULT_OK) {
 				try {
-					imatge.setImageBitmap( Media.getBitmap(getContentResolver(),
-							Uri.fromFile(tempImageFile)));
-					imatge.setRotation(90);
+					bmp = Media.getBitmap(getContentResolver(),
+							Uri.fromFile(tempImageFile));
+					canviarOrientacio();
+					imatge.setImageBitmap(bmp);
+					imatgeFeta = true;
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
@@ -122,7 +154,18 @@ public class MainActivity extends Activity implements OnClickListener {
 			}
 		}
 	}
-	
+
+	public void canviarOrientacio() {
+		if (bmp.getWidth() > bmp.getHeight()) {
+			Matrix matrix = new Matrix();
+			matrix.postRotate(90);
+			bmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(),
+					bmp.getHeight(), matrix, true);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			bmp.compress(CompressFormat.JPEG, 70, bos);
+		}
+	}
+
 	private File crearFitxer() {
 		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
 				.format(new Date());
